1c0115bca8bab87569cfc2743eecaf3e7063549d
chnliquan <chnliquan@163.com>
2021-01-16 12:06:16 +0800

    fix: exit process when private set true

--- bin/index.js
+++ bin/index.js
@@ -3,39 +3,55 @@
 'use strict'
 
 const { Command } = require('commander')
+const chalk = require('chalk')
 const { release } = require('../lib')
 const pkg = require('../package.json')
 
-const program = new Command()
+function run() {
+  if (pkg.private) {
+    console.log(
+      chalk.redBright(
+        `This package ${pkg.name} has been marked as private, remove the 'private' field from the package.json to publish it`
+      )
+    )
+    process.exit(0)
+  }
 
-program
-  .version(pkg.version, '-v, --version', 'output the current version')
-  .option('--latest', 'output latest changelog', true)
-  .option('-t, --type <type>', 'specified repo type', 'github')
-  .option('--repo-url <repoUrl>', 'specified github repo url')
-  .option('--config <config>', 'customize conventional changelog rule')
+  const program = new Command()
 
-program.parse(process.argv)
+  program
+    .version(pkg.version, '-v, --version', 'output the current version')
+    .option('--latest', 'output latest changelog', true)
+    .option('-t, --type <type>', 'specified repo type', 'github')
+    .option('--repo-url <repoUrl>', 'specified github repo url')
+    .option('--config <config>', 'customize conventional changelog rule')
 
-const options = Object.create(null)
+  program.parse(process.argv)
 
-if (program.type) {
-  options.type = program.type
+  const options = Object.create(null)
+
+  if (program.type) {
+    options.type = program.type
+  }
+
+  if (program.latest) {
+    options.latest = program.latest
+  }
+
+  if (program.repoUrl) {
+    options.repoUrl = program.repoUrl
+  }
+
+  if (program.config) {
+    options.config = program.config
+  }
+
+  release(options)
+    .then(() => process.exit(0))
+    .catch(err => {
+      console.error(err)
+      process.exit(1)
+    })
 }
 
-if (program.latest) {
-  options.latest = program.latest
-}
-
-if (program.repoUrl) {
-  options.repoUrl = program.repoUrl
-}
-
-if (program.config) {
-  options.config = program.config
-}
-
-release(options).catch(err => {
-  console.error(err)
-  process.exit(1)
-})
+run()
